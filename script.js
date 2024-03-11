9c8278de16833c767e405d4f05ea91966a53d875
Zaif-K <159206675+Zaif-K@users.noreply.github.com>
2024-03-11 14:15:18 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-let myIntro = 5;
-let enterName = 5; 
+let enterName = prompt("Enter your name");
+let playerGuess;
+let playerFeedback = "test"
+let computerGuess
+loadDIV ();
 
-if (myIntro == enterName) { 
-    alert(myIntro );
+function randomNum() { 
+    return Math.floor(Math.random )* ( 10-1) (+1)
+}
+
+function loadDIV () {
+document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "</H1>"
+document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "<H2>Guess the number between 1 & 10</H2>"
+document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + playerFeedback
+}
+
+function enterNumber (){ 
+    playerGuess = prompt ("Enter a number")
 }
